40cb01fd19351f5afdce497edd3a8ad15c5ba69f
Moksh M <mokshm@Mokshs-MacBook-Air.local>
2023-10-09 10:21:14 +0530

    made some change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router,Routes, Route} from 'react-router-dom'; 
+import Main from './Main_Page.jsx'
+import First_Page from './First_Page.jsx'
+import Login from './Login_Page.jsx'
+import Register from './Register.jsx'
+import HomePage from './Home_Page.jsx'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <Router>  
+          <div>    
+            <Routes>  
+              <Route exact path='' element={<First_Page/>}></Route>  
+              <Route exact path='/home' element={<Main/>}></Route>   
+              <Route exact path='/login' element={<Login/>}></Route> 
+              <Route exact path='/register' element={<Register/>}></Route> 
+              <Route exact path='/HomePage' element={<HomePage/>}></Route>
+            </Routes>  
+          </div>  
+       </Router>  
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
+// code 4
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Bar from './Topnav'
+import Home_Page from './Home_Page';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import Login from './Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <div>
+    <BrowserRouter>
+    <div>
+      <Routes>
+        <Route path="/" element={<Bar/>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home_Page/>} />
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+      </Routes>
+    </div>
+    </BrowserRouter>
+    
+  </div>
+  
+);
