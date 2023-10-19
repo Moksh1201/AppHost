08e3c9530d459af2bbda766a7c99c7b3bbf8ae44
Moksh1201 <130505679+Moksh1201@users.noreply.github.com>
2023-10-19 14:35:10 +0530

    HomePage.jsx

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { auth } from './firebase'; // Import the Firebase auth instance
 import { signOut } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
+import './Loginpage.css';
 
 function HomePage() {
   const history = useNavigate();
@@ -16,10 +17,16 @@ function HomePage() {
     }
   };
 
+  useEffect(() => {
+    window.history.replaceState(null, document.title, '/');
+  }, []);
+
   return (
     <div>
       <h1>You have successfully logged in to the account</h1>
-      <button className='outB1' onClick={handleSignOut}>Sign Out</button>
+      <button className="b1" onClick={handleSignOut}>
+        Sign Out
+      </button>
     </div>
   );
 }
